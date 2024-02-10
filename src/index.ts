d98c0c55518cd852157e5cec6d6b1cee75f4db3f
MarvNC <rotrobmin@gmail.com>
2024-02-09 21:10:55 -0800

    Add latest date to Pixiv dictionary

--- src/index.ts
+++ src/index.ts
@@ -8,19 +8,36 @@ const prisma = new PrismaClient();
 async function main() {
   const allArticles = await prisma.pixivArticle.findMany();
   console.log(`Found ${allArticles.length} articles`);
+
+  // Format 0000-00-00 00:00:00
+  const latestDate = (
+    await prisma.scrapeProgress.findFirst({
+      orderBy: {
+        newestDate: 'desc',
+      },
+    })
+  )?.newestDate;
+  if (!latestDate) {
+    throw new Error(`No latest date found`);
+  }
+  const latestDateShort = latestDate?.split(' ')[0];
+
+  console.log(`Latest date: ${latestDateShort}`);
+
   await prisma.$disconnect();
 
   const dictionary = new Dictionary({
-    fileName: `pixiv.zip`,
+    fileName: `Pixiv_${latestDateShort}.zip`,
   });
 
   dictionary.setIndex({
     author: `Pixiv contributors, Marv`,
     attribution: `https://dic.pixiv.net`,
+    url: `https://github.com/MarvNC/pixiv-yomitan`,
     // TODO
-    title: `Pixiv`,
+    title: `Pixiv [${latestDateShort}]`,
     revision: `1`,
-    description: `Pixiv dictionary`,
+    description: `Article summaries scraped from pixiv, ${allArticles.length} articles included`,
   });
 
   const progressBar = new SingleBar({
@@ -40,7 +57,8 @@ async function main() {
 
   progressBar.stop();
 
-  await dictionary.export();
+  console.log(`Exporting dictionary...`);
+  await dictionary.export('dist');
 }
 
 main()
