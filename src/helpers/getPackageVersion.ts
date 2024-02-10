12be4e91e62b9a6a9f43d334c0ec2752b7e4df0e
MarvNC <rotrobmin@gmail.com>
2024-02-09 21:25:47 -0800

    Update packageJsonPath in getPackageVersion function

--- src/helpers/getPackageVersion.ts
+++ src/helpers/getPackageVersion.ts
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 
 export function getPackageVersion(): string {
-  const packageJsonPath = path.join(__dirname, '..', 'package.json');
+  const packageJsonPath = path.resolve(__dirname, '../..', 'package.json');
   const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
   return packageJson.version;
 }
