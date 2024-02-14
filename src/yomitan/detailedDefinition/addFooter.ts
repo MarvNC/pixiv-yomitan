0e5427cc0a7cacae82c75a2fc915b425506828c3
MarvNC <rotrobmin@gmail.com>
2024-02-13 17:07:27 -0800

    Remove unnecessary footers

--- src/yomitan/detailedDefinition/addFooter.ts
+++ src/yomitan/detailedDefinition/addFooter.ts
@@ -6,18 +6,10 @@ export function addFooter(
   scList: StructuredContentNode[],
   article: PixivArticle,
 ) {
-  const epoch = parseInt(article.lastScraped);
-  const date = new Date(epoch);
-  const dateString = date.toISOString().slice(0, 10);
-
   scList.push({
     tag: 'div',
-    style: {
-      textAlign: 'right',
-      marginTop: '0.4em',
-    },
     data: {
-      pixiv: 'stats',
+      pixiv: 'footer',
     },
     content: [
       {
@@ -37,28 +29,6 @@ export function addFooter(
         data: {
           pixiv: 'read-more-link',
         },
-      },
-      ' | ',
-      {
-        tag: 'span',
-        style: {
-          cursor: 'pointer',
-        },
-        content: `👁 ${article.view_count}`,
-        title: `${dateString ? dateString + 'の' : ''}閲覧数`,
-      },
-      ' | ',
-      {
-        tag: 'span',
-        style: {
-          cursor: 'pointer',
-        },
-        content: `🖼️ `,
-      },
-      {
-        tag: 'a',
-        href: `https://www.pixiv.net/tags.php?tag=${article.tag_name}`,
-        content: `${article.illust_count}作品`,
       },
     ],
   });
