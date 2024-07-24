1dd6831b7177433053014a6b7c04a19e65217df1
MarvNC <rotrobmin@gmail.com>
2024-07-23 21:30:29 -0700

    Support auto updating

--- src/index.ts
+++ src/index.ts
@@ -20,11 +20,17 @@ export const prisma = new PrismaClient();
         description: 'Output lightweight dictionary',
         default: false,
       },
+      tagName: {
+        type: 'string',
+        description: 'Tag name of the Github release',
+        default: 'latest',
+      },
     })
     .parse();
   const pixivLight = !!argv.light;
+  const tagName = argv.tagName;
   console.log(
-    `Building dictionary with ${pixivLight ? 'light' : 'full'} mode.`,
+    `Building dictionary with ${pixivLight ? 'light' : 'full'} mode. Tag name: ${tagName}.`,
   );
 
   const devMode = isDevMode();
@@ -41,23 +47,34 @@ export const prisma = new PrismaClient();
   // YYYY-MM-DD
   const latestDateShort = new Date().toISOString().split('T')[0];
 
+  const PIXIV_ZIP_FILENAME: `${string}.zip` = `Pixiv${pixivLight ? 'Light' : ''}_${latestDateShort}.zip`;
+  const INDEX_FILENAME = `pixiv_${pixivLight ? 'light' : 'full'}_index.json`;
+  const EXPORT_FOLDER = 'dist';
+
   const dictionary = new Dictionary({
-    fileName: `Pixiv${pixivLight ? 'Light' : ''}_${latestDateShort}.zip`,
+    fileName: PIXIV_ZIP_FILENAME,
     termBankMaxSize: TERM_BANK_MAX_SIZE,
   });
 
   await addAllAssetsToDictionary(dictionary);
 
-  dictionary.setIndex({
-    author: `Pixiv contributors, Marv`,
-    attribution: `https://dic.pixiv.net`,
-    url: `https://github.com/MarvNC/pixiv-yomitan`,
-    title: `Pixiv${pixivLight ? ' Light' : ''} [${latestDateShort}]`,
-    revision: getPackageVersion(),
-    description: `Article summaries from the Pixiv encyclopedia (ピクシブ百科事典), ${allArticlesCount} articles included.${pixivLight ? ' Light mode.' : ''}
+  dictionary.setIndex(
+    {
+      author: `Pixiv contributors, Marv`,
+      attribution: `https://dic.pixiv.net`,
+      url: `https://github.com/MarvNC/pixiv-yomitan`,
+      title: `Pixiv${pixivLight ? ' Light' : ''} [${latestDateShort}]`,
+      revision: getPackageVersion(),
+      description: `Article summaries from the Pixiv encyclopedia (ピクシブ百科事典), ${allArticlesCount} articles included.${pixivLight ? ' Light mode.' : ''}
     Pixiv dumps used to build this found at https://github.com/MarvNC/pixiv-dump.
     Built with https://github.com/MarvNC/yomichan-dict-builder.`,
-  });
+      isUpdatable: true,
+      indexUrl: `https://github.com/MarvNC/pixiv-yomitan/releases/latest/download/${INDEX_FILENAME}`,
+      downloadUrl: `https://github.com/MarvNC/pixiv-yomitan/releases/${tagName}/${PIXIV_ZIP_FILENAME}`,
+    },
+    EXPORT_FOLDER,
+    INDEX_FILENAME,
+  );
 
   const progressBar = new SingleBar({
     format:
